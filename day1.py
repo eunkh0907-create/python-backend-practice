1910bd91d1896977a413eb96bbc03cd380b646f6
GitHubbarden_min <eunkh0907@gmail.com>
2026-01-22 20:09:26 +0900

    Day1: python basic practice

---
+++ day1.py
@@ -0,0 +1,15 @@
+#출력
+print("Hello Pyton")
+
+#변수
+name = "barden"
+age = 33
+
+print(name)
+print(age)
+
+name = input("이름을 입력하세요: ")
+age = int(input("나이를 입력하세요: "))
+
+print("이름:", name)
+print("나이:", age)
