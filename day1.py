71c24e5828826882244a950c9f3270b64289c7fd
GitHubbarden_min <eunkh0907@gmail.com>
2026-01-22 20:20:11 +0900

    Day1 : add 10 years output

--- day1.py
+++ day1.py
@@ -1,15 +1,13 @@
-#출력
-print("Hello Pyton")
-
-#변수
-name = "barden"
-age = 33
-
-print(name)
-print(age)
+# Day 1: 입력 -> 처리 -> 출력
 
 name = input("이름을 입력하세요: ")
-age = int(input("나이를 입력하세요: "))
+birth_year_str = input("태어난 연도(예:2000): ")
+birth_year = int(birth_year_str)
 
-print("이름:", name)
-print("나이:", age)
+this_year = 2026
+age = this_year - birth_year
+
+print("----- 결과 -----")
+print("이름: ", name)
+print("올해 기준 나이: ", age)
+print("10년 뒤 나이: ", age + 10)
